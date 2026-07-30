77d077bcacf9dae08015ab5ef590b956aa2c9100
ipedronmartins-hue <ipedronmartins@gmail.com>
2026-07-30 17:08:45 +0100

    feat: add mock weekly workout plan

---
+++ lib/mockWeeklyPlan.ts
@@ -0,0 +1,7 @@
+export const mockWeeklyPlan=[
+{day:'Mon',focus:'Strength'},
+{day:'Tue',focus:'Recovery'},
+{day:'Wed',focus:'Hypertrophy'},
+{day:'Thu',focus:'Cardio'},
+{day:'Fri',focus:'Full Body'}
+]
