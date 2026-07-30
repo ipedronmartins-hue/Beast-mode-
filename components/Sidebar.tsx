cc5607fdb2d8366697a0b6b26f888278f5f6abc0
ipedronmartins-hue <ipedronmartins@gmail.com>
2026-07-30 12:48:31 +0100

    feat: add sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,16 @@
+export default function Sidebar(){
+ return (
+  <aside>
+   <h2>Beast Mode</h2>
+   <nav>
+    <ul>
+     <li>Dashboard</li>
+     <li>Athletes</li>
+     <li>Workouts</li>
+     <li>Calendar</li>
+     <li>Reports</li>
+    </ul>
+   </nav>
+  </aside>
+ )
+}
