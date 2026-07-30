17557af3daba07630b963017dc1eac35e9f4316f
ipedronmartins-hue <ipedronmartins@gmail.com>
2026-07-30 17:03:42 +0100

    feat: add mock achievements

---
+++ lib/mockAchievements.ts
@@ -0,0 +1,3 @@
+import type { Achievement } from '../types/achievement'
+
+export const mockAchievements: Achievement[]=[{id:'1',title:'Primeiro Treino',description:'Conclui o primeiro treino.',unlocked:true,icon:'🏅'}]
